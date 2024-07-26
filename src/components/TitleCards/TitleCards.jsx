f72964f512ce6441b98934804a7c5fbaa1e3e75d
DJGankstar <117345527+DJGankstar@users.noreply.github.com>
2024-07-26 17:26:01 +1000

    Display movie data from TMDB API

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data';
 
@@ -7,7 +7,18 @@ import cards_data from '../../assets/cards/Cards_data';
 const TitleCards = ({title, category}) => {
 
 
+  const[apiData, setApiData] = useState([]);
   const cardsRef = useRef();
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhN2RlN2MyZGQzMWQzMDgyOWU1NDMzNzlhN2Q0YTc5MiIsIm5iZiI6MTcyMTk3ODA2NC45NDE4ODgsInN1YiI6IjY2YTM0NzM3M2RlMjEwMjExMjA4ZjFiNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.k-pk1nGAavFw9-IVCfDgohTZE-F5e1GMeiY4Mo3mPc0'
+    }
+  };
+  
+ 
 
   const handleWheel = (event) => {
     event.preventDefault();
@@ -15,6 +26,12 @@ const TitleCards = ({title, category}) => {
   }
 
   useEffect(() => {
+
+    fetch(`https://api.themoviedb.org/3/movie/${category?category:"now_playing"}?language=en-US&page=1`, options)
+    .then(response => response.json())
+    .then(response => setApiData(response.results))
+    .catch(err => console.error(err));
+
     cardsRef.current.addEventListener('wheel', handleWheel);
 
   }, [])
@@ -23,12 +40,12 @@ const TitleCards = ({title, category}) => {
     <div className = "titlecards">
       <h2>{title?title: "Popular On Netflix"}</h2>
       <div className = "card-list" ref = {cardsRef}>
-        {cards_data.map( (card, index) => {
+        {apiData.map( (card, index) => {
           return(
             <div className = "card" key = {index}>
-              <img src = {card.image} alt = "" />
+              <img src = {`https://image.tmdb.org/t/p/w500/` + card.backdrop_path} alt = "" />
               <div className = "card-info">
-                <p>{card.name}</p>
+                <p>{card.original_title}</p>
               </div>
             </div>
           )
